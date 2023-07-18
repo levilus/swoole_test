34a3306b41fad8c0d4348ee3818af08855998a77
levilus <139854093+levilus@users.noreply.github.com>
2023-07-18 09:20:31 +0000

    3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function show()
     {
         $template = new Template();
-        $template->parse("Hello, {{ name }}!")->render([
+        echo $template->parse("Hello, {{ name }}!")->render([
             'name' => 'John Doe'
         ]);
     }
