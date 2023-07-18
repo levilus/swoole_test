a2d311e09d0899b1f7218cc9fe797f7f94ec22a0
levilus <139854093+levilus@users.noreply.github.com>
2023-07-18 09:01:03 +0000

    2

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/user', [UserController::class, 'show']);
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/user', [UserController::class, 'show']);
